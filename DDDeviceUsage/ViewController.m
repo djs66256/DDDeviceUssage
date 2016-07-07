116e7cd4b9e020a96741e5ec74336d6305f57f97
Daniel <djs66256@163.com>
2016-07-07 18:33:47 +0800

    Initial Commit

---
+++ DDDeviceUsage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DDDeviceUsage
+//
+//  Created by hzduanjiashun on 16/7/7.
+//  Copyright © 2016年 Daniel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
